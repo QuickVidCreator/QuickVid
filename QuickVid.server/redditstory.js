00658e615729c0d932f38a89bfca63f03d1f19ec
Connor <Connor@DESKTOP-RIBIGCE>
2025-03-18 12:47:46 -0400

    b

--- QuickVid.server/redditstory.js
+++ QuickVid.server/redditstory.js
@@ -11,7 +11,17 @@ const getMP3Duration = require('get-mp3-duration'); // Added this import
 const { Readable } = require('stream'); // Use require for consistency
 const textToSpeech = require('@google-cloud/text-to-speech');
 const fsp = require('fs').promises;
+const logFile = fs.createWriteStream('output.log', { flags: 'a' });
 
+console.log = function (message) {
+    logFile.write(`${new Date().toISOString()} - ${message}\n`);
+    process.stdout.write(`${message}\n`);
+};
+
+console.error = function (message) {
+    logFile.write(`${new Date().toISOString()} - ERROR: ${message}\n`);
+    process.stderr.write(`${message}\n`);
+};
 
 const mime = require('mime-types');
 const cors = require('cors');
@@ -97,7 +107,10 @@ async function processRedditStory(req, res) {
         format = info.formats.find(format => format.qualityLabel === '1440p60' && format.container === 'mp4');
     }
     if (!format) {
-        const format = ytdl.chooseFormat(info.formats, {
+        format = info.formats.find(format => format.qualityLabel === '1440p' && format.container === 'mp4');
+    }
+    if (!format) {
+        format = ytdl.chooseFormat(info.formats, {
             quality: 'highestvideo',
             container: 'mp4'
         });
