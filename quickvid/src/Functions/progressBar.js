448573c0a03c9231eb7c39111df86d924cd09ce5
Connor <Connor@DESKTOP-RIBIGCE>
2025-04-02 14:27:09 -0400

    g

---
+++ quickvid/src/Functions/progressBar.js
@@ -0,0 +1,22 @@
+export function progressBarFunction() {
+    let progress = 0.0;
+    setShowProgress(true);
+
+    const interval = setInterval(() => {
+        progress += (progress < 0.7 ? 0.0015 : (progress < 0.8 ? 0.0010 : 0.0007));
+        setProgressValue(progress);
+
+        if (progress >= 1) {
+            clearInterval(interval);
+        }
+    }, 50); // Runs every 100ms
+
+    // Stop the progress when the file is received
+    setProgressValue(prev => {
+        if (prev >= 1) {
+            clearInterval(interval);
+            return 1;
+        }
+        return prev;
+    });
+}
